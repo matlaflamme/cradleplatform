e2af51397b27f78fec6d88943846f8870063e9e5
pnaddaf <parmisnaddaf@yahoo.com>
2019-09-24 17:21:10 -0700

    added v-model to all the classes in patientSummary

---
+++ webapp/js/patientSummary.js
@@ -0,0 +1,13 @@
+let patientSummary = new Vue({
+    el: '#patientSummary',
+    data: {
+        clinicName : '',
+        clinicLocation : '',
+        clinicPhoneNum : ''
+    },
+    methods: {
+        submitClinic() {
+            //create a new health clinic
+        }
+    }
+});
