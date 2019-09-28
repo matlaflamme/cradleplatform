cdecb9116e55a5a093b589735842e69e848f51a3
jsa275 <jsa275@sfu.ca>
2019-09-28 14:36:35 -0700

    Changes user table schema from user_id to username

--- web/scripts/db-initializer-scripts/schema.sql
+++ web/scripts/db-initializer-scripts/schema.sql
@@ -13,7 +13,7 @@ USE cradlerest;
 -- For mock api testing: NOT FOR PRODUCTION USE!!!
 CREATE TABLE user (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL
 );
@@ -44,7 +44,6 @@ CREATE TABLE reading (
     timestamp DATETIME NOT NULL,
     FOREIGN KEY (pid) REFERENCES patient (id)
 );
-
 
 -- Dummy Data
 
