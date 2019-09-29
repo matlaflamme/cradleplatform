95faaacd91dcbe7199d00f5113be09a912ee22fa
mlaflamm <mathieu_laflamme@sfu.ca>
2019-09-29 14:11:41 -0700

    [FIX]: Spacing on home.html & added permissions

--- web/src/main/java/com/cradlerest/web/service/config/WebSecurityConfig.java
+++ web/src/main/java/com/cradlerest/web/service/config/WebSecurityConfig.java
@@ -65,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				// Disable security on all "/api" routes (for testing)
 				.antMatchers("/api/**").permitAll()
 				.antMatchers("/login*").permitAll()
+				.antMatchers("/home*").permitAll()
 				.anyRequest().authenticated()
 				.and()
 				.formLogin()
