cde279a8be028c179cd6ead3639ddf19c7ded283
Jeremy Schwartz <jdschwar@sfu.ca>
2019-09-29 17:37:45 -0700

    [ADD]: Run Integration Tests Powershell Script

---
+++ cradletest/run-tests.ps1
@@ -0,0 +1,27 @@
+#
+# Convenience script for running the web server's integration tests.
+# Requires docker-compose.exe
+#
+# Returns a non-zero exit code if the tests fail, returns exit code 0 iff all
+# tests pass.
+#
+
+if ((Split-Path -Leaf -Path $PWD) -ne "cradletest") {
+    Write-Error "Script must be run from 'cradletest' directory"
+}
+
+# Compile web server to ensure we are testing the latest version.
+Push-Location ..\web
+Write-Host "Compiling web server..."
+.\mvnw.cmd package -DskipTests | Out-Null
+if ($LastExitCode -ne 0) {
+    Write-Error "Compilation Failed: aborting integration tests"
+    Pop-Location
+    exit 1
+}
+Write-Host "Compilation Successful!"
+Pop-Location
+
+
+# Run integration tests within docker.
+return docker-compose.exe up --build --abort-on-container-exit --exit-code-from tester
