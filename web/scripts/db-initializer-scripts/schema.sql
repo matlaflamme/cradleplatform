1088372182be718709b131bdb6fda60f6c7acc7d
Jeremy Schwartz <jdschwar@sfu.ca>
2019-10-07 13:28:43 -0700

    [FIX]: Formatting In schema.sql

--- web/scripts/db-initializer-scripts/schema.sql
+++ web/scripts/db-initializer-scripts/schema.sql
@@ -45,23 +45,59 @@ CREATE TABLE reading (
     FOREIGN KEY (pid) REFERENCES patient (id)
 );
 
+
+
 -- Dummy Data
 -- To pre encode:
 -- https://www.browserling.com/tools/bcrypt
 INSERT INTO user (username, password, role) -- password: admin
-VALUES ('admin', '{bcrypt}$2a$10$1aYZ3QtMTK9k930s54ROmumfKskM1xLM1UznBgzjx4Mxn4tseLS4i', 'ROLE_ADMIN,ROLE_VHT,ROLE_HEALTHWORKER');
+VALUES (
+    'admin',
+    '{bcrypt}$2a$10$1aYZ3QtMTK9k930s54ROmumfKskM1xLM1UznBgzjx4Mxn4tseLS4i',
+    'ROLE_ADMIN,ROLE_VHT,ROLE_HEALTHWORKER'
+
+);
 INSERT INTO user  (username, password, role) -- password: health
-VALUES ('health', '{bcrypt}$2a$10$q26.1T6krvjYHT9Me3sqqeQld9uPviBqxVphDLuDLX8octw3iV8hC', 'ROLE_HEALTHWORKER');
+VALUES (
+    'health',
+    '{bcrypt}$2a$10$q26.1T6krvjYHT9Me3sqqeQld9uPviBqxVphDLuDLX8octw3iV8hC',
+    'ROLE_HEALTHWORKER'
+);
+
 INSERT INTO user  (username, password, role)
-VALUES ('vht', '{bcrypt}$2a$10$MMMLYEsDhbgEXRSg9agAL.NwSoXTcjfed980ClKAFu/nl/ol9W5xG', 'ROLE_VHT');
+VALUES (
+    'vht',
+    '{bcrypt}$2a$10$MMMLYEsDhbgEXRSg9agAL.NwSoXTcjfed980ClKAFu/nl/ol9W5xG',
+    'ROLE_VHT'
+);
+
 INSERT INTO user  (username, password, role)
-VALUES ('test', '{bcrypt}$2a$10$MOX4VmxWhj0rBEeD8JYFaODweciGexZggq0jiqGNBWIoPQZTf6KB2', 'ROLE_ADMIN');
+VALUES (
+    'test',
+    '{bcrypt}$2a$10$MOX4VmxWhj0rBEeD8JYFaODweciGexZggq0jiqGNBWIoPQZTf6KB2',
+    'ROLE_ADMIN'
+);
+
 INSERT INTO user  (username, password, role)
-VALUES ('adminvht', '{bcrypt}$2a$10$cDqOL/UNe5.4zGdR9EkMJOmUEq4RDuROMp.VFUAFBJX.tfGorrc/a', 'ROLE_VHT,ROLE_ADMIN');
+VALUES (
+    'adminvht',
+    '{bcrypt}$2a$10$cDqOL/UNe5.4zGdR9EkMJOmUEq4RDuROMp.VFUAFBJX.tfGorrc/a',
+    'ROLE_VHT,ROLE_ADMIN'
+);
+
 INSERT INTO user  (username, password, role)
-VALUES ('vhthealth', '{bcrypt}$2a$10$V1w/4w9OAtvKT00Q92rPp.iuxO4s43HOKppp5BPSXT1OnIDFG.KgC', 'ROLE_VHT,ROLE_HEALTH');
+VALUES (
+    'vhthealth',
+    '{bcrypt}$2a$10$V1w/4w9OAtvKT00Q92rPp.iuxO4s43HOKppp5BPSXT1OnIDFG.KgC',
+    'ROLE_VHT,ROLE_HEALTH'
+);
+
 INSERT INTO user  (username, password, role)
-VALUES ('adminhealth', '{bcrypt}$2a$10$Ytx8e2oCF7oidLZA.Bmi0.dtTZorDpijhmYV69sMyUIRKxfpw1rpy', 'ROLE_ADMIN,ROLE_HEALTH');
+VALUES (
+    'adminhealth',
+    '{bcrypt}$2a$10$Ytx8e2oCF7oidLZA.Bmi0.dtTZorDpijhmYV69sMyUIRKxfpw1rpy',
+    'ROLE_ADMIN,ROLE_HEALTH'
+);
 
 
 INSERT INTO patient
