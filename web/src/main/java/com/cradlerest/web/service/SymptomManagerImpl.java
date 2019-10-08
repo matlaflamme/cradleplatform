2dd533060e96e82be5cb89d7512e006041854945
Jeremy Schwartz <jdschwar@sfu.ca>
2019-10-07 22:10:12 -0700

    [FIX]: Compilation Issues After Merge

--- web/src/main/java/com/cradlerest/web/service/SymptomManagerImpl.java
+++ web/src/main/java/com/cradlerest/web/service/SymptomManagerImpl.java
@@ -1,6 +1,6 @@
 package com.cradlerest.web.service;
 
-import com.cradlerest.web.controller.error.EntityNotFoundException;
+import com.cradlerest.web.controller.exceptions.EntityNotFoundException;
 import com.cradlerest.web.model.Symptom;
 import com.cradlerest.web.service.repository.SymptomRepository;
 import org.jetbrains.annotations.NotNull;
