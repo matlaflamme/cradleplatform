4e9af4c4cebbd816bcb27a64942db4d5e686bc0c
Jeremy Schwartz <jdschwar@sfu.ca>
2019-10-08 20:51:13 -0700

    [ADD]: Initial Data Generation Tool

---
+++ web/src/main/java/com/cradlerest/web/util/datagen/GenerateDummyData.java
@@ -0,0 +1,49 @@
+package com.cradlerest.web.util.datagen;
+
+import com.cradlerest.web.util.datagen.error.MissingAnnotationException;
+import org.reflections.Reflections;
+
+import java.util.Set;
+
+/**
+ * Application entry point for the dummy data generation tool.
+ *
+ * Accepts a single, optional command line argument which is used as the seed
+ * for the random number generator allowing for the reproduction of data.
+ */
+public class GenerateDummyData {
+
+	private static final String SEARCH_PACKAGE = "com.cradlerest.web";
+
+	public static void main(String[] args) {
+		try {
+			var entities = getAllEntityTypes();
+			for (var entity : entities) {
+				System.out.println(entity.getName());
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * Returns the set of all database entity classes in the "com.cradlerest.web"
+	 * package. These classes should have a one-to-one correspondence to the
+	 * database schema allowing us to generate dummy data based on their structure.
+	 *
+	 * @return A set of entity classes.
+	 */
+	private static Set<Class<?>> getAllEntityTypes() throws MissingAnnotationException {
+		Reflections.log = null;
+		var reflections = new Reflections(SEARCH_PACKAGE);
+		var entities = reflections.getTypesAnnotatedWith(javax.persistence.Entity.class);
+		for (var entity : entities) {
+			if (!entity.isAnnotationPresent(javax.persistence.Table.class)) {
+				throw MissingAnnotationException.type(entity, javax.persistence.Table.class);
+			}
+		}
+
+		return entities;
+	}
+}
