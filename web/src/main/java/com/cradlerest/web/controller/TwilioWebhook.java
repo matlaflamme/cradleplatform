8b9621d104c31067510eea156cb5b9eed2beed08
jsa275 <jsa275@sfu.ca>
2019-10-12 23:00:23 -0700

    Adds twilio account settings

--- web/src/main/java/com/cradlerest/web/controller/TwilioWebhook.java
+++ web/src/main/java/com/cradlerest/web/controller/TwilioWebhook.java
@@ -1,7 +1,21 @@
 package com.cradlerest.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
 public class TwilioWebhook {
+
+	@PostMapping("/sms")
+	public void respondToSmS(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		request
+		System.out.println("sms");
+		return "textmsg received";
+	}
 }
