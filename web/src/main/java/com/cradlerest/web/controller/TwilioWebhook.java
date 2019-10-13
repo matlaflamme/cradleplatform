c3c708db903e8087355ebe58343a2db5f2b2c6f0
jsa275 <jsa275@sfu.ca>
2019-10-13 03:19:27 -0700

    Adds twilio filter

--- web/src/main/java/com/cradlerest/web/controller/TwilioWebhook.java
+++ web/src/main/java/com/cradlerest/web/controller/TwilioWebhook.java
@@ -15,41 +15,16 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
- * Handles HTTP requests from Twilio
+ * Used to test Twilio webhook
+ * currently using: /api/twilio/uploadsms
  *
- * Twilio src
- * https://github.com/twilio
- *
- * {
- *     "sid": "SM722f2af6f0c048e2b2b336222c236f19",
- *     "date_created": "Sun, 13 Oct 2019 06:25:20 +0000",
- *     "date_updated": "Sun, 13 Oct 2019 06:25:20 +0000",
- *     "date_sent": null,
- *     "account_sid": "AC6cf5e5d23357bc99a1d39777f20647d8",
- *     "to": "+17781234567",
- *     "from": "+12053465536",
- *     "messaging_service_sid": null,
- *     "body": "Sent from your Twilio trial account - hello",
- *     "status": "queued",
- *     "num_segments": "1",
- *     "num_media": "0",
- *     "direction": "outbound-api",
- *     "api_version": "2010-04-01",
- *     "price": null,
- *     "price_unit": "USD",
- *     "error_code": null,
- *     "error_message": null,
- *     "uri": "/2010-04-01/Accounts/AC6cf5e5d23357bc99a1d39777f20647d8/Messages/SM722f2af6f0c048e2b2b336222c236f19.json",
- *     "subresource_uris": {
- *         "media": "/2010-04-01/Accounts/AC6cf5e5d23357bc99a1d39777f20647d8/Messages/SM722f2af6f0c048e2b2b336222c236f19/Media.json"
- *     }
- * }
  */
 @RestController
 @RequestMapping("/api/twilio")
 public class TwilioWebhook {
 
 	/**
+	 * Returns a string as an SMS reply (this replies to the sender with a text)
 	 * https://www.twilio.com/docs/sms/twiml#twilios-request-to-your-application
 	 *
 	 * @param request Twilio post response body: https://www.twilio.com/docs/sms/twiml#twilios-request-to-your-application
@@ -59,9 +34,6 @@ public class TwilioWebhook {
 	@PostMapping("/uploadsms")
 	public String respondToSmS(@RequestParam Map<String, String> request, HttpServletResponse response) throws IOException {
 		System.out.println("received: " + request.toString());
-		Message message = new Message.Builder("Message Received").build();
-		MessagingResponse responseMessage = new MessagingResponse.Builder().message(message).build();
-
-		return responseMessage.toXml();
+		return "You: " + request.get("Body");
 	}
 }
---
+++ web/src/main/java/com/cradlerest/web/service/config/TwilioRequestValidatorFilter.java
@@ -0,0 +1,98 @@
+package com.cradlerest.web.service.config;
+
+import com.twilio.security.RequestValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Ensures twilio requests are twilio requests
+ *
+ * THIS CLASS WAS COPY AND PASTED FROM:
+ * https://www.twilio.com/docs/usage/tutorials/how-to-secure-your-servlet-app-by-validating-incoming-twilio-requests
+ */
+
+public class TwilioRequestValidatorFilter implements Filter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TwilioRequestValidatorFilter.class);
+	private RequestValidator requestValidator;
+	private String myAuthToken = "2190957f91e23511702ae9b0eb866726";
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		System.out.println("twilio filter");
+		LOGGER.info("Initiating Twilio filter");
+		requestValidator = new RequestValidator(myAuthToken);
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		boolean isValidRequest = false;
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+			// Concatenates the request URL with the query string
+			String pathAndQueryUrl = getRequestUrlAndQueryString(httpRequest);
+			// Extracts only the POST parameters and converts the parameters Map type
+			Map<String, String> postParams = extractPostParams(httpRequest);
+			String signatureHeader = httpRequest.getHeader("X-Twilio-Signature");
+
+			isValidRequest = requestValidator.validate(
+					pathAndQueryUrl,
+					postParams,
+					signatureHeader);
+		}
+
+		if(isValidRequest) {
+			chain.doFilter(request, response);
+		} else {
+			((HttpServletResponse)response).sendError(HttpServletResponse.SC_FORBIDDEN);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// Nothing to do
+	}
+
+	private Map<String, String> extractPostParams(HttpServletRequest request) {
+		String queryString = request.getQueryString();
+		Map<String, String[]> requestParams = request.getParameterMap();
+		List<String> queryStringKeys = getQueryStringKeys(queryString);
+
+		return requestParams.entrySet().stream()
+				.filter(e -> !queryStringKeys.contains(e.getKey()))
+				.collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()[0]));
+	}
+
+	private List<String> getQueryStringKeys(String queryString) {
+		if(queryString == null || queryString.length() == 0) {
+			return Collections.emptyList();
+		} else {
+			return Arrays.stream(queryString.split("&"))
+					.map(pair -> pair.split("=")[0])
+					.collect(Collectors.toList());
+		}
+	}
+
+	private String getRequestUrlAndQueryString(HttpServletRequest request) {
+		String queryString = request.getQueryString();
+		String requestUrl = request.getRequestURL().toString();
+		if(queryString != null && queryString != "") {
+			return requestUrl + "?" + queryString;
+		}
+		return requestUrl;
+	}
+}
