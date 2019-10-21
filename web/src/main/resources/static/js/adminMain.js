e9d3265cb1c6de935ec70bcaafd4e695043bf229
mlaflamm <mathieu_laflamme@sfu.ca>
2019-10-21 00:35:09 -0700

    [FIX]: SideBar items turned into components. Fixes #87

--- web/src/main/resources/static/js/adminMain.js
+++ web/src/main/resources/static/js/adminMain.js
@@ -30,6 +30,29 @@ Vue.component('info_card', {
         '</v-card>'
 });
 
+Vue.component('sidebar_item', {
+    props: {
+        title: String,
+        icon: String,
+        clickable: String
+    },
+    template:
+    '<v-list-item @click="changeWindow(clickable)">' +
+        '<v-list-item-action>' +
+            '<v-icon>{{icon}}</v-icon>' +
+        '</v-list-item-action>' +
+        '<v-list-item-content>' +
+            '<v-list-item-title>{{title}}</v-list-item-title>' +
+        '</v-list-item-content>' +
+    '</v-list-item>',
+    methods: {
+        changeWindow: function(clicked) { //ideally, a click should call a different function based on prop 'clickable'
+            //for now: if/else with clicked value to change window
+            console.log("change window to " + clicked);
+        },
+    }
+});
+
 
 new Vue({
     el: '#app',
