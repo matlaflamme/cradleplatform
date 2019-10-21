3371406493ed681ce4d39601719047347f5f6c13
jsa275 <jsa275@sfu.ca>
2019-10-20 17:07:02 -0700

    add keys to readingColour enum

--- web/src/main/java/com/cradlerest/web/model/ReadingColour.java
+++ web/src/main/java/com/cradlerest/web/model/ReadingColour.java
@@ -13,9 +13,28 @@ import com.fasterxml.jackson.annotation.JsonFormat;
  */
 @JsonFormat(shape = JsonFormat.Shape.NUMBER)
 public enum ReadingColour {
-	GREEN,
-	YELLOW_DOWN,
-	YELLOW_UP,
-	RED_DOWN,
-	RED_UP
+	GREEN(0),
+	YELLOW_DOWN(1),
+	YELLOW_UP(2),
+	RED_DOWN(3),
+	RED_UP(4);
+
+	private final int key;
+
+	ReadingColour(int key) {
+		this.key = key;
+	}
+
+	public int getKey() {
+		return this.key;
+	}
+
+	public static ReadingColour fromKey(int key) {
+		for(ReadingColour type : ReadingColour.values()) {
+			if(type.getKey() == key) {
+				return type;
+			}
+		}
+		return null;
+	}
 }
