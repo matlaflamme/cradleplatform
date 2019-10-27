ae9d3875ac9f20e389b7995ba2b6f840d7d8186d
Jonathan Antepyan <jonathan_antepyan-ruckenstein@sfu.ca>
2019-10-27 07:34:32 -0700

    Revert "Merge branch 'revert-e3cc1091' into 'master'"
    This reverts merge request !94

--- web/src/main/java/com/cradlerest/web/service/config/MvcConfig.java
+++ web/src/main/java/com/cradlerest/web/service/config/MvcConfig.java
@@ -3,7 +3,6 @@ package com.cradlerest.web.service.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,11 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/vht").setViewName("vht");
-		registry.addViewController("/healthworker").setViewName("healthworker");
-		registry.addViewController("/admin").setViewName("admin");
 		registry.addViewController("/patientSummary").setViewName("patientSummary");
 		registry.addViewController("/createNewReading").setViewName("createNewReading");
 		registry.addViewController("/createAccount").setViewName("createAccount");
