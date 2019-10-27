4f3ef99394935c6f6ab642d1f612fe33c4b939f4
jsa275 <jsa275@sfu.ca>
2019-10-26 20:56:13 -0700

    Fixes referral table schema

--- web/scripts/db-initializer-scripts/3-schema.sql
+++ web/scripts/db-initializer-scripts/3-schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE referral (
     health_centre_number VARCHAR(255),
     comments VARCHAR(9001),
     timestamp DATETIME NOT NULL,
-    closed DATETIME default false,
+    closed DATETIME,
     accepter VARCHAR(255) default null
 );
 
