c1e88f82fbb077a6f400338f071be3298a3ba666
jsa275 <jsa275@sfu.ca>
2019-11-03 00:25:23 -0700

    Revert "Adds user created and modified timestamps"
    This reverts commit eca1a53f28864834c54318ebf7eb4ecbc6f2bc5d.

--- web/scripts/db-initializer-scripts/3-schema.sql
+++ web/scripts/db-initializer-scripts/3-schema.sql
@@ -9,9 +9,7 @@ CREATE TABLE user (
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
-    active boolean not null default 1,
-    created DATETIME not null default current_timestamp,
-    modified DATETIME on update current_timestamp
+    active boolean not null default 1
 );
 
 CREATE TABLE patient (
