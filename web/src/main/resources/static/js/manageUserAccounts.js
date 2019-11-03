2acbdfe1297d33326007b59eaa7057918cbcac5e
jsa275 <jsa275@sfu.ca>
2019-11-03 01:45:10 -0700

    admin can see all users page

--- web/src/main/resources/static/js/manageUserAccounts.js
+++ web/src/main/resources/static/js/manageUserAccounts.js
@@ -5,26 +5,94 @@ Vue.component('users_table' , {
     props: {
 
     },
-    data: () => ({
-        headers: [ //Value is the key for the items (in html, it'll use this key to know what column data will go in)
+    data () {
+        return {
+            search: '',
+            headers: [ //Value is the key for the items (in html, it'll use this key to know what column data will go in)
                 { text: 'Username', align: 'left', value: 'username'},
-                { text: 'User Role', value: 'role' },
-                { text: 'Actions', value: 'actions'},
-        ],
+                { text: 'Role', value: 'roles' },
+                { text: 'Created', value: 'created'},
+                { text: 'Modified', value: 'modified'},
+                { text: 'Active', value: 'active', sortable: false},
+            ],
             rows: [] //empty to start
-
-    }),
-    methods: {
+        }
+    },
+    mounted() { //sends request to server. Puts response into the rows variable
+        console.log("mounted");
+        this.getUsers();
 
     },
+    methods: {
+        polling: function() {
+            this.polling = setInterval(() => {
+                console.log("polling users");
+                this.getUsers();
+            }, 5000) // 5 seconds
+        },
+        getUsers: function() {
+            axios.get('/api/user/all').then(response => {
+                console.log(response.data[0]);
+                this.rows = response.data;
+            }).catch(error => {
+                console.log(error);
+            })
+        },
+
+        updateActive: function(user) {
+            console.log("clicked: " + user.username);
+        },
+
+        isActive: function(user) {
+            if (user.active) {
+                return 'green';
+            } else {
+                return 'red';
+            }
+        }
+    },
+
+    beforeDestroy() {
+        clearInterval(this.polling);
+    },
+
+    created() {
+        this.polling()
+    },
+
     template:
-    `<v-data-table
+    `
+    <template>
+    <v-card>
+    <v-card-title>All Users<v-spacer></v-spacer>
+        <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        ></v-text-field>
+        </v-card-title>
+    <v-data-table
         :headers="headers"
         :items="rows"
-        :items-per-page="5"
+        :search="search"
         class="elevation-1"
         >
-        </v-data-table>`
+        <template slot="rows" slot-scope="props">
+            <td>{{props.row.username}}</td>
+            <td>{{props.row.roles}}</td>
+            <td>{{props.row.created}}</td>
+            <td>{{props.row.modified}}</td>
+            <template v-slot:item.active="{ item }">
+                <v-btn v-if="item.active == true" color="red" v-on:click="updateActive(item)">Disable</v-btn>
+                <v-btn v-if="item.active == false" color="green" v-on:click="updateActive(item)">Enable</v-btn>
+            </template>
+        </template>
+        </v-data-table>
+        </v-card>
+        </template>
+        `
     ,
 });
 
