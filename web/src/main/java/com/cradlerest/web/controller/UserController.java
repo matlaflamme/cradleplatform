b9666cf42b5c6f3284451e1d673b7727276a2c00
Jeremy Schwartz <jdschwar@sfu.ca>
2019-11-09 17:30:37 -0800

    [ADD]: Auth Who-Am-I Tests

--- web/src/main/java/com/cradlerest/web/controller/UserController.java
+++ web/src/main/java/com/cradlerest/web/controller/UserController.java
@@ -5,12 +5,14 @@ import com.cradlerest.web.controller.exceptions.AlreadyExistsException;
 import com.cradlerest.web.controller.exceptions.DatabaseException;
 import com.cradlerest.web.controller.exceptions.EntityNotFoundException;
 import com.cradlerest.web.model.User;
+import com.cradlerest.web.model.UserDetailsImpl;
 import com.cradlerest.web.service.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -107,6 +109,29 @@ public class UserController {
 			userRepository.deleteById(id);
 		} catch (Exception e) {
 			throw new EntityNotFoundException(id);
+		}
+	}
+
+	/**
+	 * A test API method which returns the username of the requesting user.
+	 * @return The requesting user's username.
+	 */
+	@GetMapping("/whoami")
+	public Object whoAmI() {
+		class Result {
+			public String username;
+
+			private Result(String username) {
+				this.username = username;
+			}
+		}
+
+		var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal instanceof UserDetailsImpl) {
+			var userDetails = (UserDetailsImpl) principal;
+			return new Result(userDetails.getUsername());
+		} else {
+			return principal;
 		}
 	}
 }
