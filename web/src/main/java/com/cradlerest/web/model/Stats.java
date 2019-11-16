0b204388e911215d503f9dc8e887bfc44c6fd435
Quince <qbielka@sfu.ca>
2019-11-15 18:47:17 -0800

    Add the Stats Data Class
    Stats Data class and constructors

--- web/src/main/java/com/cradlerest/web/model/Stats.java
+++ web/src/main/java/com/cradlerest/web/model/Stats.java
@@ -1,4 +1,42 @@
 package com.cradlerest.web.model;
 
 public class Stats {
+    private int numberOfReadings;
+    private int numberOfReds;
+    private int numberOfGreens;
+    private int numberOfYellows;
+    private int numberOfPatientsSeen;
+    private int numberOfVHTs;
+    private int numberOfReadingsTaken;
+
+
+    public Stats(
+            int numberOfReadings,
+            int numberOfReds,
+            int numberOfGreens,
+            int numberOfYellows,
+            int numberOfPatientsSeen,
+            int numberOfVHTs,
+            int numberOfReadingsTaken
+    ) {
+        this.numberOfReadings = numberOfReadings;
+        this.numberOfReds = numberOfReds;
+        this.numberOfGreens = numberOfGreens;
+        this.numberOfYellows = numberOfYellows;
+        this.numberOfPatientsSeen = numberOfPatientsSeen;
+        this.numberOfVHTs = numberOfVHTs;
+        this.numberOfReadingsTaken = numberOfReadingsTaken;
+    }
+
+    public Stats() {
+        this.numberOfReadings = 0;
+        this.numberOfReds = 0;
+        this.numberOfGreens = 0;
+        this.numberOfYellows = 0;
+        this.numberOfPatientsSeen = 0;
+        this.numberOfVHTs = 0;
+        this.numberOfReadingsTaken = 0;
+    }
+
+
 }
