691078b8ff2f634394d0165258c6453214bab8f3
jsa275 <jsa275@sfu.ca>
2019-11-16 01:53:01 -0800

    test

--- web/src/main/java/com/cradlerest/web/controller/TwilioLogsController.java
+++ web/src/main/java/com/cradlerest/web/controller/TwilioLogsController.java
@@ -5,16 +5,11 @@ import com.twilio.Twilio;
 import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.monitor.v1.Alert;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URL;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -25,15 +20,15 @@ import java.util.List;
  *
  * Returns ALL logs from the beginning of time
  */
+
 @RestController
 @RequestMapping("/api/twilio")
-public class TwilioController {
+public class TwilioLogsController {
 	@Value("${twilio.account_sid}")
 	private String account_sid;
 
 	@Value("${twilio.auth_token}")
 	private String auth_token;
-
 
 	/**
 	 * Fetches SMS logs from Twilio account (security details above)
