4b80d272b0169c7269f065be2ae5c9906ca5f600
mlaflamm <mathieu_laflamme@sfu.ca>
2019-11-16 23:21:20 -0800

    [ADD]: Add initial chart using vue-apexcharts

--- web/src/main/resources/static/js/adminMain.js
+++ web/src/main/resources/static/js/adminMain.js
@@ -30,6 +30,31 @@ Vue.component('info_card', {
         '</v-card>'
 });
 
+Vue.component('mychart', {
+    template:
+    '<div>\n' +
+        '<apexchart width="500" type="bar" :options="chartOptions" :series="series"></apexchart>\n' +
+    '</div>',
+    components: {
+        apexchart: VueApexCharts,
+    },
+    data: function() {
+        return {
+            chartOptions: {
+                chart: {
+                    id: 'vuechart-example'
+                },
+                xaxis: {
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                }
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 35, 50, 49, 60, 70, 91]
+            }]
+        }
+    }
+});
 
 new Vue({
     el: '#app',
