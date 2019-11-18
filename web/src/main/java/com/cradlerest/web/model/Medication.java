a70fdc9a8db8715eb359ef5684e3792a564cc52a
Quince <qbielka@sfu.ca>
2019-11-18 12:33:24 -0800

    Finish Frame for Medication class

--- web/src/main/java/com/cradlerest/web/model/Medication.java
+++ web/src/main/java/com/cradlerest/web/model/Medication.java
@@ -5,4 +5,45 @@ public class Medication {
     private String medication;
     private String dosage;
     private String usageFrequency;
+
+    public Medication(int id, String medication, String dosage, String usageFrequency) {
+        this.id = id;
+        this.medication = medication;
+        this.dosage = dosage;
+        this.usageFrequency = usageFrequency;
+    }
+
+    public Medication(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getUsageFrequency() {
+        return usageFrequency;
+    }
+
+    public void setUsageFrequency(String usageFrequency) {
+        this.usageFrequency = usageFrequency;
+    }
 }
