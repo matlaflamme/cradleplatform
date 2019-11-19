2588c07ad98b67007a35cbbabc4d27c9b7ef0589
jantepya <jantepya@sfu.ca>
2019-11-19 00:32:34 -0800

    [ADD] diagnosis class

--- web/src/main/java/com/cradlerest/web/controller/ReferralController.java
+++ web/src/main/java/com/cradlerest/web/controller/ReferralController.java
@@ -42,7 +42,7 @@ public class ReferralController {
 	 *
 	 *
 	 * {
-	 *	 "referrerUserId": "",
+	 *	 "referrerUserName": "",
 	 *   "healthCentrePhoneNumber": ""
 	 * 	 "reading": {
 	 * 	 	 "patientId": "001",
--- web/src/main/java/com/cradlerest/web/model/Diagnosis.java
+++ web/src/main/java/com/cradlerest/web/model/Diagnosis.java
@@ -1,4 +1,71 @@
 package com.cradlerest.web.model;
 
+import com.cradlerest.web.util.datagen.annotations.DataGenAmount;
+import com.cradlerest.web.util.datagen.annotations.ForeignKey;
+import com.cradlerest.web.util.datagen.annotations.Generator;
+import com.cradlerest.web.util.datagen.impl.*;
+
+import javax.persistence.*;
+
+/**
+ * Defines a diagnosis
+ *
+ * 	For each referral, a diagnosis can be made
+ */
+@Entity
+@Table(name = "diagnosis")
+@DataGenAmount(3)
 public class Diagnosis {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	@Generator(AutoIncrementGenerator.class)
+	private Integer id;
+
+	@Column(name = "patient", nullable = false)
+	@ForeignKey(Patient.class)
+	private String patientId;
+
+	@Column(name = "description", nullable = false)
+	@Generator(GibberishSentenceGenerator.class)
+	private String description;
+
+	@Column(name = "resolved", nullable = false)
+	private Boolean resolved;
+
+
+	Diagnosis() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPatientId() {
+		return this.patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setResolved(Boolean resolved) {
+		this.resolved = resolved;
+	}
+
+	public Boolean getResolved() {
+		return resolved;
+	}
 }
