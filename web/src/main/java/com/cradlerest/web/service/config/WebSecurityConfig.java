a9dd26f025d2c0dbfa3d76f3c617803a2dcb84b6
Jeremy Schwartz <jdschwar@sfu.ca>
2019-11-23 20:06:31 -0800

    [FIX]: Authentication For Referral List
    VHTs will now only see the referrals that they have made and health
    workers can only see the referrals which are made to their health centre.

--- web/src/main/java/com/cradlerest/web/service/config/WebSecurityConfig.java
+++ web/src/main/java/com/cradlerest/web/service/config/WebSecurityConfig.java
@@ -92,12 +92,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				// VHT/Health Worker endpoints
 				.antMatchers("/api/patient/**").hasAnyRole("HEALTHWORKER", "VHT")
 				.antMatchers("/api/reading/**").hasAnyRole("HEALTHWORKER", "VHT")
+				.regexMatchers("/api/referral(?:/.+)?/all").hasAnyRole("HEALTHWORKER", "VHT")
 
 				// VHT only endpoints
 				.antMatchers("/api/referral/send/**").hasRole("VHT")
-
-				// Health worker only endpoints
-				.regexMatchers("/api/referral(?:/.+)?/all").hasRole("HEALTHWORKER")
 
 				// Deny any other request
 				.antMatchers("/api/**").denyAll()
